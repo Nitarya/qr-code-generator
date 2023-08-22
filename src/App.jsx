4c982ace09ff0bb6762096b4b0c43c72ae4b437d
Nitya <niittss27@gmail.com>
2023-08-22 18:33:05 +0530

    QR Code Generator created with react

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+// import './App.css'
+
+function App() {
+  const [temporary, setTemporary] = useState("");
+  const [word, setWord] = useState("");
+  const [size, setSize] = useState(400);
+  const [bgColor, setBgColor] = useState("ffffff");
+  const [qrCode, setOrCode] = useState("");
+
+  useEffect(() => {
+    setOrCode(
+      `https://api.qrserver.com/v1/create-qr-code/?data=${word}&size=${size}x${size}&bgcolor=${bgColor}`
+    );
+  }, [word, size, bgColor]);
+
+  function handleClick() {
+    setWord(temporary);
+  }
+
+  return (
+    <div className="App">
+      <h1>OR Code Generator</h1>
+      <div className="input-box">
+        <div className="gen">
+          <input
+            type="text"
+            onChange={(e) => {
+              setTemporary(e.target.value);
+            }}
+            placeholder="Enter text to encode"
+          />
+          <button className="button" onClick={handleClick}>
+            Generate
+          </button>
+        </div>
+        <div className="extra">
+          <h5>Background Color:</h5>
+          <input
+            type="color"
+            onChange={(e) => {
+              console.log(e.target.value.substring(1))
+              setBgColor(e.target.value.substring(1));
+            }
+          }
+          />
+          <h5>Dimension:</h5>
+          <input
+            type="range"
+            min="200"
+            max="600"
+            value={size}
+            onChange={(e) => {
+              setSize(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+      <div className="output-box">
+        <img src={qrCode} alt="" />
+        <a href={qrCode} download="QRCode">
+          <button type="button">Download</button>
+        </a>
+      </div>
+    </div>
+  );
+}
+export default App;
